aa676aa2176d4517b1eac7ac4ab266b09eb76f73
Justin DeMaris <justin@urbint.com>
2017-04-27 16:46:54 -0400

    fix(sentry): adding bearer token authorization to Sentry API requests

--- sentry/client.go
+++ sentry/client.go
@@ -27,10 +27,15 @@ func (c *client) CreateRelease(msg *Release) error {
 	body, _ := json.Marshal(msg)
 	buf := bytes.NewReader(body)
 
-	resp, err := http.Post(
-		"https://sentry.io/api/0/projects/" + c.org + "/" + c.project + "/releases",
-		"application/json",
-		buf)
+	req, err := http.NewRequest("POST",
+		"https://sentry.io/api/0/projects/" + c.org + "/" + c.project + "/releases", buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer " + c.api_key)
+	resp, err := http.DefaultClient.Do(req)
+
 	if err != nil {
 		return err
 	}
@@ -50,16 +55,22 @@ func (c *client) CreateDeploy(msg *Deploy) error {
 	body, _ := json.Marshal(msg)
 	buf := bytes.NewReader(body)
 
-	resp, err := http.Post(
+	req, err := http.NewRequest("POST",
 		"https://sentry.io/api/0/organizations/" + c.org +
-			"/releases/" + msg.Name + "/deploys",
-		"application/json",
-		buf)
+			"/releases/" + msg.Name + "/deploys", buf)
+	if err != nil {
+		return err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Authorization", "Bearer " + c.api_key)
+	resp, err := http.DefaultClient.Do(req)
+
 	if err != nil {
 		return err
 	}
 
-	if resp.StatusCode != 201 {
+	// 201 = Created, 208 = Already Exists
+	if resp.StatusCode != 201 && resp.StatusCode != 208 {
 		t, _ := ioutil.ReadAll(resp.Body)
 		return &Error{resp.StatusCode, string(t)}
 	}
